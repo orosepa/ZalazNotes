f39655ced891b13cd4515a877902705d3b0cce91
orosepa <l.olga.podgornaya@gmail.com>
2022-06-20 19:57:49 +0300

    Change colors

--- app/src/main/java/com/example/zalazmap/presentation/MapScreen.kt
+++ app/src/main/java/com/example/zalazmap/presentation/MapScreen.kt
@@ -9,16 +9,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.zalazmap.R
+import com.example.zalazmap.ui.theme.Green800
+import com.example.zalazmap.ui.theme.Purple500
+import com.example.zalazmap.ui.theme.Purple700
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.*
 import com.google.maps.android.compose.*
@@ -74,6 +78,7 @@ fun MapScreen(
                             }",
                     context = LocalContext.current,
                     iconResourceId = R.drawable.ic_baseline_circle_24,
+                    color = if (station.isExplored) Green800.toArgb() else Purple700.toArgb(),
                     onClick = {
                         coroutineScope.launch {
                             cameraPositionState.animate(
@@ -101,11 +106,12 @@ fun MapMarker(
     state: MarkerState,
     title: String,
     snippet: String,
+    color: Int,
     onClick: (Marker) -> Boolean,
     @DrawableRes iconResourceId: Int
 ) {
     val icon = bitmapDescriptorFromVector(
-        context, iconResourceId
+        context, iconResourceId, color
     )
     MarkerInfoWindow(
         state = state,
@@ -121,14 +127,12 @@ fun MapMarker(
                 .background(Color(245, 227, 215))
                 .padding(8.dp),
         ) {
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(
-                    text = title,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    fontSize = 20.sp
-                )
-            }
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = snippet)
         }
@@ -137,12 +141,15 @@ fun MapMarker(
 
 fun bitmapDescriptorFromVector(
     context: Context,
-    vectorResId: Int
+    vectorResId: Int,
+    color: Int
 ): BitmapDescriptor? {
 
     // retrieve the actual drawable
     val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
     drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    drawable.setTint(color)
+
     val bm = Bitmap.createBitmap(
         drawable.intrinsicWidth,
         drawable.intrinsicHeight,
